2a27b2396cbf68ac18c17da36ec1ae1dbbc6a662
Oleg Suvorov <olegsuv@gmail.com>
2023-01-26 20:39:29 +0100

    Code compression

--- src/components/post-slider/index.tsx
+++ src/components/post-slider/index.tsx
@@ -14,11 +14,15 @@ import { Render } from "./render";
 
 function PostSlider() {
   const [posts, setPosts] = useState<Post[] | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<Error | null>(null);
   const [postNumber, setPostNumber] = useState<number>(0);
   const [medias, setMedias] = useState(new Map());
   const [users, setUsers] = useState(new Map());
+
+  const errorHandler = (error: Error) => {
+    console.error(error);
+    setError(error);
+  };
 
   //storing json data for media and users as Map objects
   const updateMedias = useCallback(
@@ -34,76 +38,32 @@ function PostSlider() {
     [users]
   );
 
-  //fetch Media json for post
-  const fetchMediaPromise = (mediaId: string) =>
-    fetchMedia(mediaId)
-      .then((data: MediaResponse) => {
-        const { media } = data.response;
-        return media;
+  //preloading image as Promise
+  const loadImage = useCallback(
+    (url: string) =>
+      new Promise((resolve, reject) => {
+        const img = new Image();
+        img.addEventListener("load", () => resolve(img));
+        img.addEventListener("error", (err) => reject(err));
+        img.src = url;
       })
-      .catch((error) => {
-        console.error(error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-  //fetch User json for post
-  const fetchUserPromise = (userName: string) =>
-    fetchUser(userName)
-      .then((data: UserResponse) => {
-        const { user } = data.response;
-        return user;
-      })
-      .catch((error) => {
-        console.error(error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-
-  //going to fetch them once on mount and loop
-  const fetchPostsPromise = useCallback(
-    () =>
-      fetchPosts()
-        .then((data: PostsResponse) => {
-          const firstPost = data.response.posts[0];
-          (async function () {
-            await getNewMedia(firstPost.mediaId);
-            await getNewUser(firstPost.user.username);
-            setPosts(data.response.posts);
-          })();
+        .then(() => {
+          return true;
         })
-        .catch((error) => {
-          console.error(error);
-          setError(error);
-        })
-        .finally(() => {
-          setLoading(false);
-        }),
+        .catch(errorHandler),
     []
   );
 
-  //preloading image as Promise
-  const loadImage = (url: string) =>
-    new Promise((resolve, reject) => {
-      const img = new Image();
-      img.addEventListener("load", () => resolve(img));
-      img.addEventListener("error", (err) => reject(err));
-      img.src = url;
-    })
-      .then(() => {
-        return true;
-      })
-      .catch((err) => console.error(err));
-
   //checking current map and request new json + image in case for media
-  const getNewMedia = async (newMediaId: string) => {
+  const getNewMedia = useCallback(async (newMediaId: string) => {
     if (newMediaId && !medias.has(newMediaId)) {
       //insert media json data into Map
-      const newMedia = await fetchMediaPromise(newMediaId);
+      const newMedia = await fetchMedia(newMediaId)
+        .then((data: MediaResponse) => {
+          const { media } = data.response;
+          return media;
+        })
+        .catch(errorHandler);
       newMedia && updateMedias(newMediaId, newMedia);
 
       //preload new media image
@@ -112,13 +72,18 @@ function PostSlider() {
       await loadImage(newImageUrl);
     }
     return;
-  };
+  }, []);
 
   //checking current map and request new json + image in case for user
-  const getNewUser = async (newUsername: string) => {
+  const getNewUser = useCallback(async (newUsername: string) => {
     if (newUsername && !users.has(newUsername)) {
       //insert user json data into Map
-      const newUser = await (() => fetchUserPromise(newUsername))();
+      const newUser = await fetchUser(newUsername)
+        .then((data: UserResponse) => {
+          const { user } = data.response;
+          return user;
+        })
+        .catch(errorHandler);
       newUser && updateUsers(newUsername, newUser);
 
       //preload new user image
@@ -127,44 +92,53 @@ function PostSlider() {
       await loadImage(newImageUrl);
     }
     return;
-  };
-
-  //we need (for the first time only) media json + media image + user json + user image and then show
-  const onTimeToChange = async () => {
-    const postsAvailable = posts?.length || 0;
-    const isLastPost = postNumber >= postsAvailable - 1;
-    const getNewPostNumber = isLastPost ? 0 : postNumber + 1;
-    const newPost = posts?.[getNewPostNumber];
-    newPost && (await getNewMedia(newPost.mediaId));
-    newPost && (await getNewUser(newPost.user.username));
-    setPostNumber(getNewPostNumber);
-  };
-
-  //timeout here as set time is a minimal view of slide, then loading jsons/images happening, then show of next slide
-  const useTimeout = useCallback(() => {
-    const timeout = setTimeout(onTimeToChange, POSTS_INTERVAL);
-    return () => clearTimeout(timeout);
   }, []);
 
   useEffect(() => {
-    fetchPostsPromise();
+    fetchPosts()
+      .then((data: PostsResponse) => {
+        const firstPost = data.response.posts[0];
+        (async function () {
+          await getNewMedia(firstPost.mediaId);
+          await getNewUser(firstPost.user.username);
+          setPosts(data.response.posts);
+        })();
+      })
+      .catch(errorHandler);
   }, []);
 
-  useEffect(useTimeout, [posts, postNumber]);
+  useEffect(() => {
+    const timeout = setTimeout(async () => {
+      const postsAvailable = posts?.length || 0;
+      const isLastPost = postNumber >= postsAvailable - 1;
+      const getNewPostNumber = isLastPost ? 0 : postNumber + 1;
+      const newPost = posts?.[getNewPostNumber];
+      newPost && (await getNewMedia(newPost.mediaId));
+      newPost && (await getNewUser(newPost.user.username));
+      setPostNumber(getNewPostNumber);
+    }, POSTS_INTERVAL);
+    return () => clearTimeout(timeout);
+  }, [posts, postNumber]);
+
+  if (error) {
+    return <h1>Error happen</h1>;
+  }
 
   const currentPost = posts?.[postNumber];
   const currentImage = medias.get(currentPost?.mediaId);
   const currentUser = users.get(currentPost?.user.username);
-  return error ? (
-    <h1>Error happen</h1>
-  ) : (
-    <Render
-      currentPost={currentPost!}
-      currentImage={currentImage}
-      currentUser={currentUser}
-      loading={loading}
-    />
-  );
+
+  if (currentPost) {
+    return (
+      <Render
+        currentPost={currentPost}
+        currentImage={currentImage}
+        currentUser={currentUser}
+      />
+    );
+  }
+
+  return <>Loading...</>;
 }
 
 export default PostSlider;
